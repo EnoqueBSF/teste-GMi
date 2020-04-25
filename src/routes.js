a65d4a83ab0929479004b52e39f9d4605ed790f9
Enoque Belmiro dos Santos Filho <enoquebelmiro@hotmail.com>
2020-04-25 00:10:03 -0300

    Deploy

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({message: 'Hello World'})
+  return res.json([{razao: "Enoque Belmiro", codCli: "C09999", cnpj: "99.999.999/0002-99", senha: "123456", uf:"PB", cidade: "João Pessoa", vendedor: "DIEGO SILVA", codVendedor: "99999", emailCadastro: "enoque.filho@gmimportacao.com.br", verAcesso: "0"}])
 });
 
 module.exports = routes;
